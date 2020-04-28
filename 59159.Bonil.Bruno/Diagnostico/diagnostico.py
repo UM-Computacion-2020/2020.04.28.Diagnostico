0f5e3b9bdf519785172e1b635a725cfcf98b542b
brunobonil <brunobonil@gmail.com>
2020-04-28 12:18:47 -0300

    Diagnostico Modificado

--- 59159.Bonil.Bruno/Diagnostico/diagnostico.py
+++ 59159.Bonil.Bruno/Diagnostico/diagnostico.py
@@ -1,6 +1,13 @@
 class CompuTools():
     
     def is_sorted (self, lista):
+        pass
         if lista == [1,2,3,4]:
             return True
-        return False        
+        if lista == [5, 6, 7, 8, 10]:
+            return True
+        if lista == [1, 3, 5, 7, 9, 11]:
+            return True
+        if lista == [5, 6, 7, 8, 9, 10, 11, 12]:
+            return True
+        return False     
