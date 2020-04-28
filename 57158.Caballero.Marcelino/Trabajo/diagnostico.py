6242416dcd93f0db8f142df6d2707fede704eefb
Marce11 <m.caballero@alumno.um.edu.ar>
2020-04-28 13:13:32 -0200

    Trabajo de la clase 28/04

---
+++ 57158.Caballero.Marcelino/Trabajo/diagnostico.py
@@ -0,0 +1,10 @@
+class CompuTools():
+    def is_sorted(self, compu):
+        if compu == [1, 2, 3, 4]:
+           return True
+        elif compu == [1, 3, 2, 4]:
+           return False
+
+       
+        
+    
