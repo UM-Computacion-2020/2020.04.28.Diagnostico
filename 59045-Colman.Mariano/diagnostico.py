46cddbe0c062686f31dd7141e532f5e560112027
Mariano Colman <marianito_1998_ma@hotmail.com>
2020-05-04 23:27:19 -0300

    arreglo test diagnostico

--- 59045-Colman.Mariano/diagnostico.py
+++ 59045-Colman.Mariano/diagnostico.py
@@ -1,9 +1,11 @@
 class CompuTools():
 
     def is_sorted(self, listaTest):
-        if listaTest == [1, 2, 3, 4]:
-            return True
-        else:
-            return False
-        
-
+        n = len(listaTest) # Cuenta la cantidad de numeros de la lista
+        for j in range(n-1): 
+            if listaTest[j] < listaTest[j+1]: # Compara si un elemento de la lista es menor a su posterior 
+                resultado = True
+            else:
+                resultado = False
+                break # Debe realizar un break ya que si detecta un resultado falso no es necesario continuar
+        return resultado
